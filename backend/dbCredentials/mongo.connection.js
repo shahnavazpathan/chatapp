83af2d9e4aeb45aaa4dbf239a8479e813875a533
shahnavazkhan <bca2023shahnavazkhan1852@tnraocollege.org>
2025-01-18 21:11:38 +0530

    third

--- backend/dbCredentials/mongo.connection.js
+++ backend/dbCredentials/mongo.connection.js
@@ -2,10 +2,7 @@ import mongoose from "mongoose";
 
 function connectToDB(){
 mongoose
-  .connect("mongodb://localhost:27017/chatapp", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect("mongodb://localhost:27017/chatapp")
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Connection failed", err));
 }
